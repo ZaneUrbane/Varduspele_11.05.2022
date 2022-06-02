76aa3650a45464041aa745027dade0546b234d5c
ZaneUrbane <92846630+ZaneUrbane@users.noreply.github.com>
2022-06-02 14:05:05 +0300

    ;

--- spele.js
+++ spele.js
@@ -1,14 +1,14 @@
-document.addEventListener("domcontentLoaded",()=>{
+document.Varduspele_11.05.2022 ("workspace.code-workspace",() => {
     createSquares();
   
-    function createSquares(){
-       const gameBoard = document.getElementById("board");
+    function createSquares() {
+       const gameBoard = document.getElementById("board")
 
        for (let index = 0; index < 30; index++) {
-    let square = document.createElement("div");
-           square.classList.add("square");
-           square.setAttribute("id", index+1);
-           gameBoard.appendChild(square);
+         let square = document.createElement("div")
+           square.classList.add("square")
+           square.setAttribute("id", index+1)
+           gameBoard.appendChild(square)
        }
     }
 });
